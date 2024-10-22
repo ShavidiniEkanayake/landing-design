860129a408d61b0d26b4cf2ad59068738897f79f
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-10-23 04:23:14 +0530

    Navbar implementation without relevant styles

---
+++ src/pages/_app.jsx
@@ -0,0 +1,11 @@
+
+import "../styles/globals.css";
+import Home from './home';
+
+function MyApp() {
+  return (
+   <Home />
+  );
+}
+
+export default MyApp;
---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+import {NavBar} from '@/components/layout/navbar'
+
+
+function Home(){
+    return( <NavBar/>)
+}
+
+export default Home;
