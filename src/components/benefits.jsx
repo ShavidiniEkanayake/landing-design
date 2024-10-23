e24aa7f2f1bc74e7def56ea3fe1e0061061652f0
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-10-23 11:35:33 +0530

    Added styles for benefits

--- src/components/benefits.jsx
+++ src/components/benefits.jsx
@@ -1,14 +1,14 @@
 export const Benefits = () => {
-    return(
-        <div className="py-12 px-4 text-center">
-        <div className="mb-10">
-            <h1 className="text-3xl font-bold">
-                PAYMENT METHODS BUILT <span className="text-purple-600">FOR RESTAURANTS</span>
-            </h1>
-            <p className="text-gray-500 mt-4">
-                Every Time A Guest Pays With Qlub, You Earn Way More Than Just Revenue: More Tips, More Reviews, And More Data
-            </p>
-        </div>
+  return (
+    <div className="py-12 px-4 text-center">
+       <div className="mb-10 py-20">
+        <h1 className="text-5xl mb-4 uppercase"> Benefits you've always</h1>
+        <h1 className="text-5xl font-black text-purple uppercase">Dreamed of</h1>
+        <p className="text-black/50 mt-6">
+          Every Time A Guest Pays With Qlub, You Earn Way More Than Just
+          Revenue: More Tips, More Reviews, And More Data
+        </p>
+      </div>
     </div>
-    )
-}
+  );
+};
