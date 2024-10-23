1c9fe15d7de011d0bb8e3eca38672c90b9a56f20
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-10-23 10:53:06 +0530

    Added styles for take payment component

--- src/components/take-payment.jsx
+++ src/components/take-payment.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 export const TakePayment = () => {
   return (
-    <section className="bg-purple text-white py-8">
+    <section className="bg-purple text-white py-8 font-inter">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left">
           <div className="mb-4 md:mb-0">
-            <p className="text-lg md:text-xl font-light">Choose To Take Payments</p>
+            <p className="text-lg md:text-2xl font-extralight py-2">Choose To Take Payments</p>
             <p className="text-2xl md:text-3xl font-semibold">With Qlub</p>
           </div>
-          <div className="flex flex-col md:flex-row gap-8">
+          <div className="flex flex-col md:flex-row md:gap-20 gap-8">
             <div>
-              <p className="text-2xl font-semibold">Save 16 Min</p>
+              <p className="text-2xl font-semibold py-2">Save 16 Min</p>
               <p className="text-sm font-light">Per Table</p>
             </div>
             <div>
-              <p className="text-2xl font-semibold">Earn 300%</p>
+              <p className="text-2xl font-semibold py-2">Earn 300%</p>
               <p className="text-sm font-light">More Tips</p>
             </div>
             <div>
-              <p className="text-2xl font-semibold">Get 7X</p>
+              <p className="text-2xl font-semibold py-2">Get 7X</p>
               <p className="text-sm font-light">Positive Google Reviews</p>
             </div>
           </div>
