238cca546497112a5d1dc1a124a9e8ee772888c6
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-10-23 07:33:14 +0530

    Integrated components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,17 @@
 import {NavBar} from '@/components/layout/navbar'
 import {Hero} from '@/components/hero'
-
+import {Payment} from '@/components/payment'
+import {Benefits} from '@/components/benefits'
+import {Scan} from '@/components/scan'
 
 function Home(){
     return( 
     <div>
         <NavBar/>
         <Hero/>
+        <Payment/>
+        <Benefits/>
+        <Scan/>
     </div>
     )
 }
