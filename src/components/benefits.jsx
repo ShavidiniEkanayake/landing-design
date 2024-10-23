e793ca124a22aac0943d1a44a86d00a23a715828
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-10-23 23:29:56 +0530

    Changed text container

--- src/components/benefits.jsx
+++ src/components/benefits.jsx
@@ -4,7 +4,7 @@ export const Benefits = () => {
        <div className="py-20">
         <h1 className="text-4xl md:text-5xl mb-4 uppercase"> Benefits you've always</h1>
         <h1 className="text-4xl md:text-5xl font-black text-purple uppercase">Dreamed of</h1>
-        <p className="text-black/50 mt-6">
+        <p className="text-black/50 mt-6 lg:px-80">
           Every Time A Guest Pays With Qlub, You Earn Way More Than Just
           Revenue: More Tips, More Reviews, And More Data
         </p>
--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -1,52 +1,45 @@
 import Image from "next/image";
-import Payment1 from "../../public/images/payment-1.png";
-import Payment2 from "../../public/images/payment-2.png";
-import Payment3 from "../../public/images/payment-3.png";
-import Payment4 from "../../public/images/payment-4.png";
 
 export const Payment = () => {
-  const item=[
+  const items = [
     {
       title: "Payment Methods Built for Restaurants",
-      logo: "/images/payment-1.png",
+      logo: "/images/payment-1.png", 
     },
     {
-        title: "Easy Scans Instant Service",
-        logo: "/images/payment-2.png",
+      title: "Easy Scans Instant Service",
+      logo: "/images/payment-2.png",
     },
     {
-        title: "QR Scan Swift Checkout",
-        logo: "/images/payment-3.png",
+      title: "QR Scan Swift Checkout",
+      logo: "/images/payment-3.png",
     },
     {
-        title: "Improve Your Operations",
-        logo: "/images/payment-4.png",
-    }
+      title: "Improve Your Operations",
+      logo: "/images/payment-4.png",
+    },
   ];
+
   return (
     <div className="py-12 px-4 text-center font-inter">
       <div className="md:mb-10 py-10 md:py-20">
         <h1 className="text-4xl md:text-5xl mb-4">PAYMENT METHODS BUILT</h1>
         <h1 className="text-4xl md:text-5xl font-black text-purple">FOR RESTAURANTS</h1>
-        <p className="text-black/50 mt-6">
+        <p className="text-black/50 mt-6 lg:px-[25rem]">
           Every Time A Guest Pays With Qlub, You Earn Way More Than Just
           Revenue: More Tips, More Reviews, And More Data
         </p>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-6 md:mx-16">
-        <div className="flex flex-col items-center bg-white p-2 rounded-lg">
-          <Image src={Payment1} alt="Easy Scans Instant Service" />
-        </div>
-        <div className="flex flex-col items-center bg-white p-2 rounded-lg">
-          <Image src={Payment2} alt="QR Scan Swift Checkout" />
-        </div>
-        <div className="flex flex-col items-center bg-white p-2 rounded-lg">
-          <Image src={Payment3} alt="Improve Your Operations" />
-        </div>
-        <div className="flex flex-col items-center bg-white p-2 rounded-lg">
-          <Image src={Payment4} alt="Painless Payments Happy Customers" />
-        </div>
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center bg-white p-2 rounded-lg"
+          >
+            <img src={item.logo} alt={item.title}  />
+          </div>
+        ))}
       </div>
     </div>
   );
