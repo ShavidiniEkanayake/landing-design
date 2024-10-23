9d8778ade75ea98e6ac94748200e0f171db6a91f
ShavidiniEkanayake <shavidilunika10s@gmail.com>
2024-10-24 01:22:24 +0530

    Aggest alignment

--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -19,27 +19,29 @@ const items = [
 
 export const Payment = () => {
   return (
-    <div className="py-12 px-4 text-center">
-      <div className="md:mb-10 py-10 md:py-20">
-        <h1 className="text-4xl md:text-5xl mb-4">PAYMENT METHODS BUILT</h1>
-        <h1 className="text-4xl md:text-5xl font-black text-purple">
-          FOR RESTAURANTS
-        </h1>
-        <p className="text-black/50 mt-6 lg:px-[25rem]">
-          Every Time A Guest Pays With Qlub, You Earn Way More Than Just
-          Revenue: More Tips, More Reviews, And More Data
-        </p>
-      </div>
+    <div className="container">
+      <div className="py-12 text-center">
+        <div className="md:mb-10 py-10 md:py-20">
+          <h1 className="text-4xl md:text-5xl mb-4">PAYMENT METHODS BUILT</h1>
+          <h1 className="text-4xl md:text-5xl font-black text-purple">
+            FOR RESTAURANTS
+          </h1>
+          <p className="text-black/50 mt-6 lg:px-[25rem]">
+            Every Time A Guest Pays With Qlub, You Earn Way More Than Just
+            Revenue: More Tips, More Reviews, And More Data
+          </p>
+        </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mx-6 md:mx-16">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center bg-white p-2 rounded-lg"
-          >
-            <img src={item.logo} alt={item.title} />
-          </div>
-        ))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center bg-white rounded-lg"
+            >
+              <img src={item.logo} alt={item.title} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
